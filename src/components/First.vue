b15da8099f93512904fac859caeb98af7facac22
Nami128 <128nmok@gmail.com>
2020-09-18 15:29:02 +0900

    first-commit

---
+++ src/components/First.vue
@@ -0,0 +1,15 @@
+<template>
+<div>
+  <iframe src="https://docs.google.com/presentation/d/e/2PACX-1vT7moeoiQ8gXMs1NCjWoGlmzVVmLgR9DWvsmphxpdS3jeP0teeppFHBBirjhDfaCWmBbMMDP1UWXPbr/embed?start=false&loop=false&delayms=3000" frameborder="0" width="960" height="569" allowfullscreen="true" mozallowfullscreen="true" webkitallowfullscreen="true"></iframe>
+</div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+
+</style>
